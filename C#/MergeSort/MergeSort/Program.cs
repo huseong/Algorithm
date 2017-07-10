3c428010e3485c87735a028f94167f6342d0267f
huseong <huseong2@naver.com>
2017-07-10 11:48:13 +0900

    MergeSort - C#

---
+++ C#/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MergeSort {
+    class Program {
+        static void Main (string[] args) {
+            string[] input = Console.ReadLine().Split(' ');
+            int[] inputs = new int[input.Length];
+            for(int i=0; i<input.Length; i++) {
+                inputs[i] = int.Parse(input[i]);
+            }
+            printArr(mergeSort(inputs));
+        }
+
+        static void printArr(int[] arr) {
+            for(int i=0; i<arr.Length; i++) {
+                Console.Write(arr[i] + " ");
+            }
+        }
+
+        static int[] merge(int[] a, int[] b) {
+            int i = 0;
+            int j = 0;
+            int[] returnArray = new int[a.Length + b.Length];
+            int count = 0;
+            while(true) {
+                if(i == a.Length) {
+                    int len = b.Length - j;
+                    for(int k=0; k<len; k++) {
+                        returnArray[count++] = b[j + k];
+                    }
+                    break;
+                }
+                if (j == b.Length) {
+                    int len = a.Length - i;
+                    for (int k = 0; k < len; k++) {
+                        returnArray[count++] = a[i + k];
+                    }
+                    break;
+                }
+                if (a[i] < b[j]) {
+                    returnArray[count++] = a[i++];
+                } else {
+                    returnArray[count++] = b[j++];
+                }
+            }
+            return returnArray;
+        }
+        static int[] copy(int[] arr, int startIndex, int size) {
+            int[] returnArray = new int[size];
+            for(int i=0; i<size; i++) {
+                returnArray[i] = arr[startIndex + i];
+            }
+            return returnArray;
+        }
+
+        static int[] mergeSort(int[] arr) {
+            if(arr.Length > 1) {
+                int mid = arr.Length / 2;
+                return merge(mergeSort(copy(arr, 0, mid)), mergeSort(copy(arr, mid, arr.Length - mid)));
+            }
+            return arr;
+        }
+    }
+}
---
+++ C++/MergeSort/MergeSort/MergeSort.cpp
@@ -0,0 +1,23 @@
+// MergeSort.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Enter the number of array element : ";
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+    return 0;
+}
+
+static int mergeSort(int* arr) {
+
+}
+
