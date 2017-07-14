19c3f70d86f2abf0167ca3aa778a6ac8b478a841
huseong <huseong2@naver.com>
2017-07-14 09:50:53 +0900

    Counting Sort Performance Upgrade

--- C#/Counting Sort/Counting Sort/Program.cs
+++ C#/Counting Sort/Counting Sort/Program.cs
@@ -1,32 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Counting_Sort {
     class Program {
         static void Main (string[] args) {
             string[] input = Console.ReadLine().Split(' ');
-            int[] inputs = new int[input.Length];
-            for(int i=0; i<inputs.Length; i++) {
-                inputs[i] = int.Parse(input[i]);
-            }
+            int len = input.Length;
+            int[] inputs = new int[len];
+            inputs[0] = int.Parse(input[0]);
             int maxValue = inputs[0];
-            for(int i=1; i<inputs.Length; i++) {
-                if(maxValue < inputs[i]) {
+            for (int i=1; i<len; i++) {
+                inputs[i] = int.Parse(input[i]);
+                if (maxValue < inputs[i]) {
                     maxValue = inputs[i];
                 }
             }
             int[] count = new int[maxValue+1];
-            for(int i=0; i<inputs.Length; i++) {
+            for(int i=0; i<len; i++) {
                 count[inputs[i]]++;
             }
             for(int i=1; i<count.Length; i++) {
                 count[i] += count[i - 1];
             }
             int[] array = new int[count[maxValue]];
-            for (int i=0; i<inputs.Length; i++) {
+            for (int i=0; i<len; i++) {
                 array[(count[inputs[i]]--) - 1] = inputs[i];
             }
             for(int i=0; i<array.Length; i++) {
