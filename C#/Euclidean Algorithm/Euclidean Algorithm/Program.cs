5986a357c410657a928be4e5e35bdb0b6a20e9d4
huseong2 <huseong2@naver.com>
2017-10-10 16:23:32 +0900

    Euclidean Algorithm

---
+++ C#/Euclidean Algorithm/Euclidean Algorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Euclidean_Algorithm {
+    
+    class Program {
+        static void Main(string[] args) {
+            string[] input = Console.ReadLine().Split(' ');
+            Console.WriteLine(greatestCommonDivisor(int.Parse(input[0]), int.Parse(input[1])));
+        }
+
+        private static int greatestCommonDivisor(int a, int b) {
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            while (true) {
+                int r = a % b;
+                if (r == 0) {
+                    return b;
+                }
+                a = b;
+                b = r;
+            }
+        }
+    }
+}
